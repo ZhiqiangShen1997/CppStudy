d157b4b9d2ecfd9fc14ca4a5a987fd9c9dd45ef2
申志强 <1422537078@qq.com>
2017-11-06 01:00:28 +0800

    study cpp and code some cpp codes

---
+++ CppStudy1.cpp
@@ -0,0 +1,267 @@
+
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+class Date {
+private:
+	int year;
+	int month;
+	int day;
+	int hour;
+	int minute;
+	int second;
+	int getMonthDay(int month,int year);
+public:
+	Date(int year,int month,int day,int hour,int minute,int second);
+
+	Date(const Date& date);
+
+	void showDate();
+
+	void addSecond(int second);
+	void addMinute(int minute);
+	void addHour(int hour);
+	void addDay(int day);
+	void addMonth(int month);
+	void addYear(int year);
+
+	void reduceSecond(int second);
+	void reduceMinute(int minute);
+	void reduceHour(int hour);
+	void reduceDay(int day);
+	void reduceMonth(int month);
+	void reduceYear(int year);
+};
+Date::Date(int year=2000, int month=1, int day=1, int hour=0, int minute=0, int second=0) {
+	this->year = year;
+	this->month = month;
+	this->day = day;
+	this->hour = hour;
+	this->minute = minute;
+	this->second = second;
+}
+Date::Date(const Date& date) {
+	this->year = date.year;
+	this->month = date.month;
+	this->day = date.day;
+	this->hour = date.hour;
+	this->minute = date.minute;
+	this->second = date.second;
+}
+void Date::showDate() {
+	printf("%d-%02d-%02d %02d:%02d:%02d\n", this->year, this->month, this->day, this->hour, this->minute, this->second);
+}
+void Date::addYear(int year) {
+	this->year += year;
+	if (this->year > 9999) {
+		this->year = 2000;
+		cout << "The year overflow,reset to 2000"<<endl;
+	}
+}
+void Date::addMonth(int month) {
+	this->month += month;
+
+	while (this->month > 12) {
+		this->month -= 12;
+		this->addYear(1);
+	}
+}
+void Date::addDay(int day) {
+	this->day += day;
+	
+	int monthDay = getMonthDay(this->month, this->year);
+	
+	while (this->day > monthDay) {
+		this->addMonth(1);
+		this->day -= monthDay;
+		monthDay = getMonthDay(this->month,this->year);
+	}
+}
+void Date::addHour(int hour) {
+	this->hour += hour;
+
+	while (this->hour >=24) {
+		this->hour -= 24;
+		this->addDay(1);
+	}
+}
+void Date::addMinute(int minute) {
+	this->minute += minute;
+
+	while (this->minute >= 60) {
+		this->minute -= 60;
+		this->addHour(1);
+	}
+}
+void Date::addSecond(int second) {
+	this->second += second;
+	while (this->second >= 60) {
+		this->second -= 60;
+		this->addMinute(1);
+	}
+}
+void Date::reduceYear(int year) {
+	this->year -= year;
+	while (this->year < 1) {
+		this->year = 2000;
+		cout << "The year underflow,reset to 2000" << endl;
+	}
+}
+void Date::reduceMonth(int month) {
+	this->month -= month;
+	while (this->month <= 0) {
+		this->month += 12;
+		this->reduceYear(1);
+	}
+}
+void Date::reduceDay(int day) {
+	this->day -= day;
+	while (this->day <=0) {
+		this->reduceMonth(1);
+		int monthDay = this->getMonthDay(this->month,this->year);
+		this->day += monthDay;
+	}
+}
+void Date::reduceHour(int hour) {
+	this->hour -= hour;
+	while (this->hour < 0) {
+		this->hour += 24;
+		this->reduceDay(1);
+	}
+}
+void Date::reduceMinute(int minute) {
+	this->minute -= minute;
+	while (this->minute < 0) {
+		this->minute += 60;
+		this->reduceHour(1);
+	}
+}
+void Date::reduceSecond(int second) {
+	this->second -= second;
+	while (this->second < 0) {
+		this->second += 60;
+		this->reduceMinute(1);
+	}
+}
+int Date::getMonthDay(int month, int year) {
+	int monthDay;
+	if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+		monthDay = 31;
+	}
+	else if (month == 2) {
+		if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+			monthDay = 29;
+		}
+		else {
+			monthDay = 28;
+		}
+	}
+	else {
+		monthDay = 30;
+	}
+	return monthDay;
+}
+
+
+
+
+class singleInstanceClass {
+private:
+	static singleInstanceClass* singleInstance;
+	singleInstanceClass();
+public:
+	static singleInstanceClass* getInstance();
+	int flag;
+};
+singleInstanceClass* singleInstanceClass::singleInstance = NULL;
+
+singleInstanceClass::singleInstanceClass() {}
+
+singleInstanceClass* singleInstanceClass::getInstance(){
+	if (singleInstance == NULL) {
+		singleInstance = new singleInstanceClass();
+	}
+	return singleInstance;
+}
+
+int main(void){
+	cout << "---question one---" << endl;
+	Date date(2017,10,20,18,5,34);
+	cout << "Create a new date,print the date:" << endl;
+	date.showDate();
+	
+	Date copyDate = date;
+	cout << "Clone a date,print the clone date:" << endl;
+	copyDate.showDate();
+
+	date.addSecond(3600);
+	cout << "add 3600secs:" << endl;
+	date.showDate();
+	
+	date.addMinute(800);
+	cout << "add 800mins:" << endl;
+	date.showDate();
+
+	date.addHour(24);
+	cout << "add 24 hours:" << endl;
+	date.showDate();
+
+	date.addDay(20);
+	cout << "add 20 days:" << endl;
+	date.showDate();
+
+	date.addMonth(30);
+	cout << "add 30 months:" << endl;
+	date.showDate();
+
+	date.addYear(10);
+	cout << "add 10 years:" << endl;
+	date.showDate();
+
+	date.reduceSecond(333);
+	cout << "reduce 333 secs:" << endl;
+	date.showDate();
+
+	date.reduceMinute(34);
+	cout << "reduce 34 secs:" << endl;
+	date.showDate();
+
+	date.reduceHour(100);
+	cout << "reduce 100 hours:" << endl;
+	date.showDate();
+
+	date.reduceDay(45);
+	cout << "reduce 45 days:" << endl;
+	date.showDate();
+
+	date.reduceMonth(80);
+	cout << "reduce 80 months:" << endl;
+	date.showDate();
+
+	date.reduceYear(50);
+	cout << "reduce 50 years:" << endl;
+	date.showDate();
+	cout << endl;
+	cout << endl;
+
+	cout << "---question two---" << endl;
+	singleInstanceClass* s1 = singleInstanceClass::getInstance();
+	singleInstanceClass* s2 = singleInstanceClass::getInstance();
+	
+	s1->flag = 1;
+
+	if (s1 == s2) {
+		cout << "singleInstance-->true" << endl;
+		cout << "s1-->flag " << s1->flag << endl;
+		cout << "s2-->flag " << s2->flag << endl;
+	}
+	else {
+		cout << "singleInstance-->false" << endl;
+		cout << "s1-->flag " << s1->flag << endl;
+		cout << "s2-->flag " << s2->flag << endl;
+	}
+
+	delete s1;
+	return 0;
+}
---
+++ CppStudy2.cpp
@@ -0,0 +1,176 @@
+
+#include "stdafx.h"
+#include <iostream>
+#include<string>
+using namespace std;
+class Point;
+class Caculator;
+class People {
+private:
+	string name;
+	
+public:
+	int age;
+	People(string name, string country);
+	People(const People& p);
+	~People();
+	string getName();
+	void setName(const char* name);
+	void setName(char* name);
+	int getX(const Point& p);
+	class Son;
+};
+class People::Son {
+private:
+	string name;
+public:
+	string getName() {
+		return name;
+	}
+	friend string show(const People::Son& s);
+};
+string show(const People::Son& s) {
+	return s.name;
+}
+
+
+string People::getName(){
+	return this->name;
+}
+People::People(string name="Tom",string country="中国"):name(name){
+	
+}
+People::~People() {
+	cout << "~People" << endl;
+}
+void People::setName(const char* name) {
+	char* pName = const_cast<char *>(name);
+}
+void People::setName(char* name) {
+	const char* cName = name;
+}
+People::People(const People& p) {
+	this->name = name;
+	this->age = age;
+	cout << "People(const People& p)" << endl;
+}
+class Point {
+private:
+	int x, y;
+public:
+	friend double mod(const Point& p);
+	friend int People::getX(const Point& p);
+	friend class Caculator;
+};
+int People::getX(const Point& p) {
+	return p.x;
+}
+double mod(const Point& p) {
+	return sqrt(p.x*p.x + p.y*p.y);
+}
+class Caculator {
+public:
+	static double getDistance(const Point& p1,const Point& p2);
+};
+double Caculator::getDistance(const Point& p1,const Point& p2) {
+	return sqrt((p1.x-p2.x)*(p1.x - p2.x)+ (p1.y - p2.y)*(p1.y - p2.y));
+}
+
+/*int main()
+{
+	People p;
+	People np = People();
+	const People& pRef = p;
+	People* pArray = new People[5];
+	delete []pArray;
+	People p2(p);
+	
+	
+	int People::*pAge;
+	string(People::*pGet)();
+
+	pAge = &People::age;
+	pGet = &People::getName;
+
+	cout<<p.*pAge<<endl;
+	cout << (p.*pGet)()<<endl;
+	Point point;
+	cout<<mod(point)<<endl;
+	cout<<p.getX(point) << endl;
+	
+	cout<<Caculator::getDistance(point,point)<<endl;
+	People::Son son;
+	cout<<son.getName()<<endl;
+    return 0;
+}*/
+class Complex
+{
+private:
+	int real;
+	int imag;
+public:
+	Complex(int real, int imag);
+	Complex operator +(const Complex& c);
+	Complex operator -(const Complex& c);
+	operator double();
+	friend ostream& operator <<(ostream& o,const Complex& c);
+	friend istream& operator >>(istream& i,Complex& c);
+	Complex& operator +();
+	Complex operator -();
+	Complex operator ++(int);
+	Complex& operator ++();
+};
+
+Complex::Complex(int real, int imag) :real(real), imag(imag) {}
+
+Complex Complex::operator +(const Complex& c) {
+	return Complex(real + c.real, imag + c.imag);
+}
+Complex Complex::operator -(const Complex& c) {
+	return Complex(real - c.real, imag - c.imag);
+}
+Complex::operator double() {
+
+	return sqrt(real*real + imag*imag);
+}
+ostream& operator <<(ostream& o,const Complex& c) {
+	o << "(" << c.real <<","<<c.imag<<")"<< endl;
+	return o;
+}
+istream& operator >>(istream& i, Complex& c) {
+	i >> c.real >> c.imag;
+	return i;
+}
+Complex& Complex::operator +() {
+	return *this;
+}
+Complex Complex::operator -() {
+	return Complex(-real, -imag);
+}
+Complex Complex::operator ++(int) {
+	Complex temp=Complex(real, imag);
+	real++; imag++;
+	return temp;
+}
+Complex& Complex::operator ++() {
+	real++; imag++;
+	return *this;
+}
+int main() {
+	Complex c1(5,10);
+	Complex c2(3, 10);
+	cout << c1;
+
+	cout << c1++;
+	cout << ++c1;
+
+	cin >> c2;
+	cout << c1+c2;
+	
+	double mod = c1;
+	cout << mod << endl;
+	
+	cout << -c1;
+}
+
+
